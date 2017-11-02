8f47fe2455af61563de4e9868b1719d5ba02bd74
LUCAS-PC\lucas <gombk666@gmail.com>
2017-11-02 01:24:00 -0200

    Magias de Attack 8.7 / Scripts para Level 50/100

---
+++ data/spells/scripts/attack/ultimate terra strike.lua
@@ -0,0 +1,9 @@
+local distanceCombat = createCombatObject()
+setCombatParam(distanceCombat, COMBAT_PARAM_TYPE, COMBAT_EARTHDAMAGE)
+setCombatParam(distanceCombat, COMBAT_PARAM_EFFECT, CONST_ME_CARNIPHILA)
+setCombatParam(distanceCombat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLEARTH)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1, -10, -1, -20, 10, 10, 3.6, 3.6)
+
+function onCastSpell(cid, var)
+    return doCombat(cid, combat, var)
+end
