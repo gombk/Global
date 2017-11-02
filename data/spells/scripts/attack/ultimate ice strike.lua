3506c39fb86a96f147d08ae438bca0c2b3f92d27
LUCAS-PC\lucas <gombk666@gmail.com>
2017-11-02 01:50:53 -0200

    Spells

--- data/spells/scripts/attack/ultimate ice strike.lua
+++ data/spells/scripts/attack/ultimate ice strike.lua
@@ -1,12 +1,9 @@
-local distanceCombat = createCombatObject()
-setCombatParam(distanceCombat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
-setCombatParam(distanceCombat, COMBAT_PARAM_EFFECT, CONST_ME_ICEATTACK)
-setCombatParam(distanceCombat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLICE)
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ICEATTACK)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ICE)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1, -10, -1, -20, 10, 10, 3.6, 3.6)
 
 function onCastSpell(cid, var)
-	if(variantToNumber(var) ~= 0) then
-		return doCombat(cid, distanceCombat, var)
-	end
-	return doCombat(cid, combat, var)
+    return doCombat(cid, combat, var)
 end
