82f4cba950d0af6a650f3b5ada860e5c997cfdfc
LUCAS-PC\lucas <gombk666@gmail.com>
2017-11-03 14:40:55 -0200

    Alguns monstros - npcs - boss corrigidos

--- data/npc/scripts/Zurak.lua
+++ data/npc/scripts/Zurak.lua
@@ -14,13 +14,17 @@ function onThink() 						npcHandler:onThink() end
 local travelNode = keywordHandler:addKeyword({"chazorai"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Do you want to sail to Chazorai " .. (getConfigInfo("freeTravel") and "free?" or "50 gps?")})
 	travelNode:addChildKeyword({"yes"}, StdModule.travel, {npcHandler = npcHandler, premium = false, level = 0, cost = 0, destination = {x=33100, y=31018, z=7} })
 	travelNode:addChildKeyword({"no"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = "We would like to serve you some time."})
+
+local travelNode = keywordHandler:addKeyword({"isle of strife"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Do you want to sail to Isle of Strife " .. (getConfigInfo("freeTravel") and "free?" or "50 gps?")})
+	travelNode:addChildKeyword({"yes"}, StdModule.travel, {npcHandler = npcHandler, premium = false, level = 0, cost = 0, destination = {x=33100, y=31018, z=7} })
+	travelNode:addChildKeyword({"no"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = "We would like to serve you some time."})
 	
 local travelNode = keywordHandler:addKeyword({"back"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Do you want to sail to Back " .. (getConfigInfo("freeTravel") and "free?" or "50 gps?")})
 	travelNode:addChildKeyword({"yes"}, StdModule.travel, {npcHandler = npcHandler, premium = false, level = 0, cost = 0, destination = {x=33158, y=31228, z=7} })
 	travelNode:addChildKeyword({"no"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = "We would like to serve you some time."})
 
-keywordHandler:addKeyword({"travel"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Where do you want to go {Chazorai} or {Back}."})
-keywordHandler:addKeyword({"go"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Where do you want to go {Chazorai} or {Back}."})
+keywordHandler:addKeyword({"travel"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Where do you want to go {Chazorai}, {Isle of Strife} or {Back}."})
+keywordHandler:addKeyword({"go"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Where do you want to go {Chazorai}, {Isle of Strife} or {Back}."})
 keywordHandler:addKeyword({"job"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "I am the captain of this ship."})
 keywordHandler:addKeyword({"captain"}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "I am the captain of this ship."})
 
